c87145b13e8a973979b983a4154896c04f1d47f1
Victoria Rogozina <vika.asco@gmail.com>
2025-07-08 22:29:46 +0400

    added newTest

---
+++ src/test/java/AuthorizationTest.java
@@ -0,0 +1,66 @@
+
+package tests;
+
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+public class AuthorizationTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.holdBrowserOpen = true;
+        Configuration.timeout = 5000; // default 4000
+    }
+
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+        $("#firstName").setValue("Дейдюша");
+        $("#lastName").setValue("Рогозин");
+        $("#userEmail").setValue("rat@rat.ru");
+        $("#gender-radio-2").parent().click();
+        $("#userNumber").setValue("8005553535");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("December");
+        $(".react-datepicker__year-select").selectOption("2024");
+        $$(".react-datepicker__day").findBy(text("30")).click();
+
+        $("#subjectsInput").setValue("Arts").pressEnter();
+        $("#hobbies-checkbox-2").parent().click();
+        $("#uploadPicture").uploadFromClasspath("photo.jpg");
+        $("#currentAddress").setValue("Ratrat");
+
+        $("#state").click();
+        $(byText("NCR")).click();
+        $("#city").click();
+        $(byText("Delhi")).click();
+
+        $("#submit").click();
+
+        $(".modal-content").shouldBe(visible);
+
+        $(".table-responsive").shouldHave(text("Дейдюша"));
+        $(".table-responsive").shouldHave(text("Рогозин"));
+        $(".table-responsive").shouldHave(text("Female"));
+        $(".table-responsive").shouldHave(text("8005553535"));
+        $(".table-responsive").shouldHave(text("30 December,2024"));
+        $(".table-responsive").shouldHave(text("Arts"));
+        $(".table-responsive").shouldHave(text("Reading"));
+        $(".table-responsive").shouldHave(text("photo.jpg"));
+        $(".table-responsive").shouldHave(text("Ratrat"));
+        $(".table-responsive").shouldHave(text("NCR Delhi"));
+
+
+    }
+    }
